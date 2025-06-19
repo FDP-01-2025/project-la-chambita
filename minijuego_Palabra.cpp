9291609c4a70e76f8115e22c8f0800f04d5aa372
EscalanteRivas <00031825@uca.edu.sv>
2025-06-19 00:12:10 -0600

    Presentacion:memo:

--- minijuego_Palabra.cpp
+++ minijuego_Palabra.cpp
@@ -32,11 +32,12 @@ void mezclarPalabra(char palabra[], int length)
 // Función principal del minijuego
 bool jugarOrdenaPalabra()
 {
-    // Inicialización ventana y FPS
     InitWindow(800, 450, "Minijuego: Ordenar la palabra");
     SetTargetFPS(60);
 
-    // Lista de palabras (arreglo de char)
+    // Cargar imagen de fondo
+    Texture2D fondo = LoadTexture("wallpapper1.png");
+
     const int TOTAL_PALABRAS = 5;
     const char palabras[TOTAL_PALABRAS][20] = {
         "computadora",
@@ -45,11 +46,9 @@ bool jugarOrdenaPalabra()
         "variable",
         "funcion"};
 
-    // Elegir palabra aleatoria
     srand(time(0));
     int indice = rand() % TOTAL_PALABRAS;
 
-    // Copiar palabra original a arreglo modif.
     char palabraOriginal[20];
     int longitud = 0;
     while (palabras[indice][longitud] != '\0' && longitud < 19)
@@ -59,17 +58,14 @@ bool jugarOrdenaPalabra()
     }
     palabraOriginal[longitud] = '\0';
 
-    // Copiar para mezclar
     char palabraMezclada[20];
     for (int i = 0; i <= longitud; i++)
     {
         palabraMezclada[i] = palabraOriginal[i];
     }
 
-    // Mezclar la palabra
     mezclarPalabra(palabraMezclada, longitud);
 
-    // Variables para intento usuario
     char intentoUsuario[20] = {0};
     int intentoLen = 0;
 
@@ -79,13 +75,10 @@ bool jugarOrdenaPalabra()
     bool juegoTerminado = false;
     bool gano = false;
 
-    // Tamaño ventana para centrar textos
     const int anchoVentana = 800;
 
-    // Bucle principal del juego
     while (!WindowShouldClose() && !juegoTerminado)
     {
-        // Captura caracteres tecleados
         int key = GetCharPressed();
         if (key > 0)
         {
@@ -97,19 +90,16 @@ bool jugarOrdenaPalabra()
             }
         }
 
-        // Borrar letra con BACKSPACE
         if (IsKeyPressed(KEY_BACKSPACE) && intentoLen > 0)
         {
             intentoLen--;
             intentoUsuario[intentoLen] = '\0';
         }
 
-        // Confirmar intento con ENTER
         if (IsKeyPressed(KEY_ENTER))
         {
             intentos++;
 
-            // Comparar intento con palabra original
             bool iguales = true;
             for (int i = 0; i < longitud; i++)
             {
@@ -122,7 +112,6 @@ bool jugarOrdenaPalabra()
 
             if (iguales)
             {
-                // Ganó el jugador
                 gano = true;
                 juegoTerminado = true;
                 guardarPuntaje(palabraOriginal, intentos);
@@ -131,53 +120,49 @@ bool jugarOrdenaPalabra()
             {
                 if (intentos >= maxIntentos)
                 {
-                    // Se acabaron los intentos
                     juegoTerminado = true;
                 }
                 else
                 {
-                    // Reiniciar intento
                     intentoLen = 0;
                     intentoUsuario[0] = '\0';
                 }
             }
         }
 
-        // Comenzar a dibujar la ventana
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        // Textos centrados usando MeasureText
+        // Escalar fondo para cubrir ventana y poner transparencia
+        float escalaX = 800.0f / fondo.width;
+        float escalaY = 450.0f / fondo.height;
+        Color colorFondo = {255, 255, 255, 100}; // transparencia 100/255
+        DrawTextureEx(fondo, (Vector2){0, 0}, 0.0f, escalaX, colorFondo);
 
-        // Texto "Ordena la palabra:"
+        // Centrando textos
         const char *textoPrincipal = "Ordena la palabra:";
         int anchoTextoPrincipal = MeasureText(textoPrincipal, 20);
         int xTextoPrincipal = (anchoVentana - anchoTextoPrincipal) / 2;
 
-        // Palabra mezclada
         int anchoPalabraMezclada = MeasureText(palabraMezclada, 40);
         int xPalabraMezclada = (anchoVentana - anchoPalabraMezclada) / 2;
 
-        // Texto "Tu intento:"
         const char *textoIntento = "Tu intento:";
         int anchoTextoIntento = MeasureText(textoIntento, 20);
         int xTextoIntento = (anchoVentana - anchoTextoIntento) / 2;
 
-        // Intento usuario (texto que escribe)
         int anchoIntentoUsuario = MeasureText(intentoUsuario, 40);
         int xIntentoUsuario = (anchoVentana - anchoIntentoUsuario) / 2;
 
-        // Texto intentos restantes
         char textoIntentos[50];
         sprintf(textoIntentos, "Intentos: %d / %d", intentos, maxIntentos);
         int anchoTextoIntentos = MeasureText(textoIntentos, 20);
         int xTextoIntentos = (anchoVentana - anchoTextoIntentos) / 2;
 
-        // Dibujar todo el texto
         DrawText(textoPrincipal, xTextoPrincipal, 20, 20, DARKGRAY);
         DrawText(palabraMezclada, xPalabraMezclada, 60, 40, BLUE);
         DrawText(textoIntento, xTextoIntento, 130, 20, DARKGRAY);
-        DrawText(intentoUsuario, xIntentoUsuario, 160, 40, MAROON);
+        DrawText(intentoUsuario, xIntentoUsuario, 160, 40, DARKGREEN);
         DrawText(textoIntentos, xTextoIntentos, 220, 20, DARKGRAY);
 
         EndDrawing();
@@ -190,30 +175,38 @@ bool jugarOrdenaPalabra()
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
+        // Mismo fondo con transparencia
+        float escalaX = 800.0f / fondo.width;
+        float escalaY = 450.0f / fondo.height;
+        Color colorFondo = {255, 255, 255, 100};
+        DrawTextureEx(fondo, (Vector2){0, 0}, 0.0f, escalaX, colorFondo);
+
         if (gano)
         {
             const char *textoGano = "¡Correcto! Has ganado.";
             int anchoGano = MeasureText(textoGano, 30);
             int xGano = (anchoVentana - anchoGano) / 2;
-            DrawText(textoGano, xGano, 270, 30, GREEN);
+            DrawText(textoGano, xGano, 330, 30, DARKGREEN);
         }
         else
         {
             const char *textoPerdio = "Se acabaron los intentos.";
             int anchoPerdio = MeasureText(textoPerdio, 30);
             int xPerdio = (anchoVentana - anchoPerdio) / 2;
-            DrawText(textoPerdio, xPerdio, 270, 30, RED);
+            DrawText(textoPerdio, xPerdio, 350, 30, RED);
 
             char textoPalabra[50];
             sprintf(textoPalabra, "La palabra era: %s", palabraOriginal);
             int anchoPalabra = MeasureText(textoPalabra, 20);
             int xPalabra = (anchoVentana - anchoPalabra) / 2;
-            DrawText(textoPalabra, xPalabra, 310, 20, DARKGRAY);
+            DrawText(textoPalabra, xPalabra, 330, 20, DARKGRAY);
         }
 
         EndDrawing();
     }
 
+    UnloadTexture(fondo);
     CloseWindow();
+
     return gano;
 }
